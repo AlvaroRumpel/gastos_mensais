6cd0d55d54a3b2722ac8e513111e13cd36a7e800
Alvaro Rumpel <alvaroRumpel@gmail.com>
2022-11-01 22:04:03 -0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,24 +7,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Gastos Mensais',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
-        scaffoldBackgroundColor: Color.fromARGB(255, 53, 15, 60),
+        appBarTheme: const AppBarTheme(
+          color: Colors.purple,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w800,
+          ),
+          centerTitle: true,
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 53, 15, 60),
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home'),
     );
@@ -41,165 +42,232 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var valor = 0.0;
-  TextEditingController despesas = TextEditingController();
+  TextEditingController despesasController = TextEditingController();
   TextEditingController divididoController = TextEditingController();
   List historico = [];
   int dividido = 1;
 
+  ButtonStyle buttonStyle() => ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+          (state) {
+            if (state.contains(MaterialState.pressed)) {
+              return Colors.purple[400];
+            }
+            return Colors.purple;
+          },
+        ),
+        surfaceTintColor: MaterialStateProperty.resolveWith<Color?>(
+          (state) {
+            if (state.contains(MaterialState.pressed)) {
+              return Colors.purple[400];
+            }
+            return Colors.purple;
+          },
+        ),
+        overlayColor: MaterialStateProperty.resolveWith<Color?>(
+          (state) {
+            if (state.contains(MaterialState.pressed)) {
+              return Colors.purple[400];
+            }
+            return Colors.purple;
+          },
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            InkWell(
-              onTap: () => showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Dialog(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 8.0),
-                              child: Text(
-                                'Dividir',
-                                style: TextStyle(fontSize: 24),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () => showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(
+                                  'Dividir',
+                                  style: TextStyle(fontSize: 24),
+                                ),
                               ),
-                            ),
-                            TextField(
-                              controller: divididoController,
-                              keyboardType: TextInputType.number,
-                              decoration: const InputDecoration(
+                              TextField(
+                                controller: divididoController,
+                                keyboardType: TextInputType.number,
+                                maxLength: 2,
+                                decoration: InputDecoration(
                                   suffixText: '%',
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Porcentagem da divisão'),
-                            ),
-                            ElevatedButton.icon(
-                              icon: const Icon(Icons.add),
-                              onPressed: () {
-                                dividido =
-                                    (100 / int.parse(divididoController.text))
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  hintText: 'Porcentagem da divisão',
+                                ),
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              ),
+                              ElevatedButton.icon(
+                                icon: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  if (divididoController.text.isNotEmpty) {
+                                    dividido = (100 /
+                                            int.parse(divididoController.text))
                                         .round();
-                                setState(() {
-                                  dividido;
-                                });
-                                Navigator.pop(context);
-                              },
-                              label: const Text('Adicionar divisão'),
-                            ),
-                          ],
+                                    setState(() {
+                                      dividido;
+                                    });
+                                  }
+                                  Navigator.pop(context);
+                                },
+                                label: const Text(
+                                  'Adicionar divisão',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                style: buttonStyle(),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }),
-              child: Card(
-                elevation: 4,
-                color: Colors.deepPurple[400],
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Icon(Icons.monetization_on,
-                          size: 32, color: Colors.white),
-                      Text(
-                        'R\$ ${valor / dividido} / $dividido',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.w800,
+                      );
+                    }),
+                child: Card(
+                  elevation: 4,
+                  color: Colors.deepPurple[400],
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Icon(
+                          Icons.monetization_on,
+                          size: 32,
                           color: Colors.white,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              itemCount: historico.length,
-              itemBuilder: (BuildContext context, index) => Card(
-                color: Colors.deepPurple[200],
-                child: ListTile(
-                  onLongPress: () {
-                    valor -= double.parse(historico[index]);
-                    historico.remove(historico[index]);
-                    setState(() {
-                      historico;
-                    });
-                  },
-                  title: Text('R\$ ${historico[index]}',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      )),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showDialog(context),
-        tooltip: 'Adicionar gastos',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Future<dynamic> _showDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext) {
-          return Dialog(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(
-                      'Adicionar despesa',
-                      style: TextStyle(fontSize: 24),
+                        Text(
+                          'R\$ ${valor / dividido} / $dividido',
+                          style: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  TextField(
-                    controller: despesas,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        prefixText: 'R\$ ',
-                        border: OutlineInputBorder(),
-                        hintText: 'valor da despesa'),
-                  ),
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.add),
-                    onPressed: () {
-                      valor = valor + double.parse(despesas.text);
-                      historico.add(despesas.text);
-                      despesas.clear();
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: historico.length,
+                itemBuilder: (BuildContext context, index) => Card(
+                  color: Colors.deepPurple[200],
+                  child: ListTile(
+                    onLongPress: () {
+                      valor -= double.parse(historico[index]);
+                      historico.remove(historico[index]);
                       setState(() {
                         historico;
                       });
-                      Navigator.pop(context);
                     },
-                    label: Text('Adicionar despesas'),
+                    title: Text(
+                      'R\$ ${historico[index]}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
                   ),
-                ],
+                ),
               ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async => await _showDialog(context),
+          tooltip: 'Adicionar gastos',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showDialog(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Adicionar despesa',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+                TextField(
+                  controller: despesasController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: 'R\$ ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Valor da despesa',
+                  ),
+                ),
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    if (despesasController.text.isNotEmpty) {
+                      valor = valor + double.parse(despesasController.text);
+                      historico.add(despesasController.text);
+                      despesasController.clear();
+                      setState(() {
+                        historico;
+                      });
+                    }
+                    Navigator.pop(context);
+                  },
+                  label: const Text(
+                    'Adicionar despesas',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: buttonStyle(),
+                ),
+              ],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
